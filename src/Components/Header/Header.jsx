99989e7a1bc4db025baf1ed5e059e00fe41ea051
MdSarmad <28mdsarmad99@gmail.com>
2022-01-09 05:32:12 +0530

    Initial commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css"
+
+
+function Header() {
+    return (
+        <div className="header">
+
+            <div className="name">MD SARMAD</div>
+            <div className="navbar">
+                <div className="nav1">NAVBAR1</div>
+                <div className="nav2">NAVBAR2</div>
+                <div className="nav3">NAVBAR3</div>
+            </div>           
+        </div>
+    )
+}
+
+export default Header
